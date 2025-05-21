c55771e75ea1f74785bc5d1251584a63de3e388a
Wolf Vollprecht <w.vollprecht@gmail.com>
2025-05-21 10:09:48 +0200

    use ninja

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 mkdir build && cd build
 
-cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
+cmake ${CMAKE_ARGS} -GNinja -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DSQLITECPP_INTERNAL_SQLITE=OFF \
